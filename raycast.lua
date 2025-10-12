76cccfcdc163c9faced43c377fa381d6ee095903
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-12 20:40:45 +0300

    Update raycast.lua

--- raycast.lua
+++ raycast.lua
@@ -6,7 +6,7 @@ local DR = 0.0174533
 function DrawRays3D()
     local ra = Player.a - DR * 30  
     
-    for r = 1, 360 do
+    for r = 1, 60 do
         if ra < 0 then
             ra = ra + 2 * math.pi
         end
@@ -17,7 +17,9 @@ function DrawRays3D()
         
         local hRx, hRy, vRx, vRy = 0, 0, 0, 0
         local hHit, vHit = false, false
+        local hDist, vDist = math.huge, math.huge
         
+        -- Горизонтальные проверки
         local dofH = 0
         local aTan = -1 / math.tan(ra)
         local rxH, ryH, xoH, yoH = 0, 0, 0, 0
@@ -32,9 +34,7 @@ function DrawRays3D()
             rxH = (Player.y - ryH) * aTan + Player.x
             yoH = 64
             xoH = -yoH * aTan
-        end
-        
-        if ra == 0 or ra == math.pi then
+        else
             rxH = Player.x
             ryH = Player.y
             dofH = 8
@@ -49,6 +49,7 @@ function DrawRays3D()
                 hRx = rxH
                 hRy = ryH
                 hHit = true
+                hDist = math.sqrt((Player.x - hRx)^2 + (Player.y - hRy)^2)
                 dofH = 8
             else
                 rxH = rxH + xoH
@@ -56,7 +57,8 @@ function DrawRays3D()
                 dofH = dofH + 1
             end
         end
-	
+        
+        -- Вертикальные проверки
         local dofV = 0
         local nTan = -math.tan(ra)
         local rxV, ryV, xoV, yoV = 0, 0, 0, 0
@@ -88,6 +90,7 @@ function DrawRays3D()
                 vRx = rxV
                 vRy = ryV
                 vHit = true
+                vDist = math.sqrt((Player.x - vRx)^2 + (Player.y - vRy)^2)
                 dofV = 8
             else
                 rxV = rxV + xoV
@@ -96,29 +99,65 @@ function DrawRays3D()
             end
         end
         
-        local finalRx, finalRy
+        local finalRx, finalRy, finalDist
+        local wallColor = {0.8, 0.8, 0.8}
         
         if hHit and vHit then
-            local hDist = math.sqrt((Player.x - hRx)^2 + (Player.y - hRy)^2)
-            local vDist = math.sqrt((Player.x - vRx)^2 + (Player.y - vRy)^2)
-            
             if hDist < vDist then
-                finalRx, finalRy = hRx, hRy
+                finalRx, finalRy, finalDist = hRx, hRy, hDist
+                wallColor = {0.8, 0.8, 0.8}
             else
-                finalRx, finalRy = vRx, vRy
+                finalRx, finalRy, finalDist = vRx, vRy, vDist
+                wallColor = {0.6, 0.6, 0.6}
             end
         elseif hHit then
-            finalRx, finalRy = hRx, hRy
+            finalRx, finalRy, finalDist = hRx, hRy, hDist
+            wallColor = {0.8, 0.8, 0.8}
         elseif vHit then
-            finalRx, finalRy = vRx, vRy
+            finalRx, finalRy, finalDist = vRx, vRy, vDist
+            wallColor = {0.6, 0.6, 0.6}
         else
-            finalRx, finalRy = Player.x + math.cos(ra) * 500, Player.y + math.sin(ra) * 500
+            finalRx = Player.x + math.cos(ra) * 500
+            finalRy = Player.y + math.sin(ra) * 500
+            finalDist = 500
+            wallColor = {0.8, 0.8, 0.8}
         end
         
         love.graphics.setColor(1, 0, 0)
+        love.graphics.setLineWidth(1)
         love.graphics.line(Player.x + Player.width/2, Player.y + Player.height/2, 
-        finalRx, finalRy)
+                          finalRx, finalRy)
         
-        ra = ra +  DR
+        -- 3д сцена
+        local ca = Player.a - ra
+        
+        if ca < 0 then
+            ca = ca + 2 * math.pi
+        end
+        
+        if ca > 2 * math.pi then
+            ca = ca - 2 * math.pi
+        end
+        
+        finalDist = finalDist * math.cos(ca)
+        
+        if finalDist < 1 then
+            finalDist = 1
+        end
+        
+        local lineH = (mapS * 512) / finalDist
+        local lineO = 256 - lineH / 2
+        
+        if lineH > 1024 then 
+            lineH = 1024 
+			lineO = 256 - 512
+        end
+        
+        local lineX = r * (512 / 60)
+        love.graphics.setColor(wallColor[1], wallColor[2], wallColor[3])
+        love.graphics.setLineWidth(512 / 60 + 0.5)
+        love.graphics.line(lineX + 512, lineO, lineX + 512, lineH + lineO)
+        
+        ra = ra + DR
     end
 end
