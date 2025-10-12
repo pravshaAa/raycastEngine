84327921a0d0246b617af029a908329290ccd2c5
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-12 20:41:43 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -3,12 +3,20 @@ require"map"
 require"raycast"
 
 function love.load()
-	love.graphics.setBackgroundColor(0.3,0.3,0.3)--(0.06, 0.73, 0.9)
+	love.graphics.setBackgroundColor(0.3,0.3,0.3)--(0.06, 0.73, 0.9)  
 	Player = Player()
 end
 
 function love.update(dt)
+	
 	Player:Walk()
+	
+	for i, v in ipairs(mapCordTable) do
+		if CheckCollision(Player.x, Player.y, Player.width, Player.height, mapCordTable[i].x, mapCordTable[i].y, mapS, mapS) then
+			Player.x = Player.oldX
+			Player.y = Player.oldY
+		end
+	end
 end
 
 function love.draw()
@@ -16,3 +24,4 @@ function love.draw()
 	MapDraw()
 	Player:Draw()
 end
+
