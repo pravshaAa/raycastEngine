59526f26c20ffa4bb983bed7dd352c4f5a5841b6
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-12 20:42:47 +0300

    Update map.lua

--- map.lua
+++ map.lua
@@ -1,14 +1,15 @@
 mapX=8
 mapY=8
 mapS=64
+mapCordTable = {}
 
 map = {
 	1,1,1,1,1,1,1,1,
-	1,0,1,0,0,0,0,1,
-	1,0,1,0,0,0,0,1,
 	1,0,0,0,0,0,0,1,
+	1,0,0,0,1,0,0,1,
+	1,0,1,0,1,0,0,1,
 	1,0,0,0,0,0,0,1,
-	1,0,0,0,0,1,0,1,
+	1,0,1,0,1,1,0,1,
 	1,0,0,0,0,0,0,1,
 	1,1,1,1,1,1,1,1
 }
@@ -21,6 +22,7 @@ function MapDraw()
 		if map[i] == 1 then
 			love.graphics.setColor(1,1,1)
 			love.graphics.rectangle("fill", lastX, levelMapY, mapS, mapS)
+			table.insert(mapCordTable, {x=lastX, y=levelMapY})
 		end
 		if i % mapX == 0 then
 			lastX = -mapS
