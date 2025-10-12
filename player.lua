16f1313a3b478ae0fa4f031a2d0a1da5d262c55a
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-12 20:42:33 +0300

    Update player.lua

--- player.lua
+++ player.lua
@@ -10,8 +10,14 @@ function Player()
 		speed = 2,
 		width = 8,
 		height = 8,
+		oldX = 0,
+		oldY = 0,
 		
 		Walk = function(self, dt)
+			
+			self.oldX = self.x
+			self.oldY = self.y
+			
 			if love.keyboard.isDown("w") then
 				self.x = self.x + self.dX
 				self.y = self.y + self.dY
@@ -41,11 +47,19 @@ function Player()
 			love.graphics.setColor(0.9,0.9,0)
 			love.graphics.rectangle("fill", self.x, self.y, self.width, self.height)
 			
+			love.graphics.setLineWidth(1)
 			love.graphics.line(self.x + self.width/2, self.y + self.height/2, 
 			self.x + self.width/2 + self.dX*10, self.y + self.height/2 + self.dY*10)
 		end,
 
 	}
+end
+
+function CheckCollision(x1,y1,w1,h1,x2,y2,w2,h2) 
+	return x1 < x2+w2 and
+         x2 < x1+w1 and
+         y1 < y2+h2 and
+         y2 < y1+h1 
 end
 
 return Player
