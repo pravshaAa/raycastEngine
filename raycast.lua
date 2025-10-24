165325cfb01e1cf224578f0fad78d652d277632a
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-24 16:59:25 +0300

    Update raycast.lua

--- raycast.lua
+++ raycast.lua
@@ -1,20 +1,26 @@
 require"player"
 require"map"
+require"loadTexture"
 
 local XRender3D = 512
 local Width3D = 512
 local FOV = 60
 local Height3D = 512
-local MapTileSize = 64
+local MapTileSize = mapS
 local MaxLength = 500
-local detail = 5
+local detail = 2
 
 local DR = 0.0174533 / detail
 local HalfHeight3D = Height3D / 2
 local LineWidth = Width3D / FOV / detail + 0.5
 local MaxDof = math.max(mapX, mapY)
 
+local textureSize = 64
+local wallColor = {}
+
 function DrawRays3D()
+	local PlayerXcenter = Player.x + Player.width/2
+	local PlayerYcenter = Player.y + Player.height/2
     local ra = Player.a - DR * (FOV/2) * detail
     
     for r = 1, FOV * detail do
@@ -29,6 +35,7 @@ function DrawRays3D()
         local hRx, hRy, vRx, vRy = 0, 0, 0, 0
         local hHit, vHit = false, false
         local hDist, vDist = math.huge, math.huge
+        local hWallType, vWallType = 0, 0
         
         -- Горизонтальные проверки
         local dofH = 0
@@ -36,18 +43,18 @@ function DrawRays3D()
         local rxH, ryH, xoH, yoH = 0, 0, 0, 0
         
         if ra > math.pi then -- Вверх
-            ryH = (math.floor(Player.y / MapTileSize) * MapTileSize) - 0.0001
-            rxH = (Player.y - ryH) * aTan + Player.x
+            ryH = (math.floor(PlayerYcenter / MapTileSize) * MapTileSize) - 0.0001
+            rxH = (PlayerYcenter - ryH) * aTan + PlayerXcenter
             yoH = -MapTileSize
             xoH = -yoH * aTan
         elseif ra < math.pi then -- Вниз
-            ryH = (math.floor(Player.y / MapTileSize) * MapTileSize) + MapTileSize
-            rxH = (Player.y - ryH) * aTan + Player.x
+            ryH = (math.floor(PlayerYcenter / MapTileSize) * MapTileSize) + MapTileSize
+            rxH = (PlayerYcenter - ryH) * aTan + PlayerXcenter
             yoH = MapTileSize
             xoH = -yoH * aTan
         else
-            rxH = Player.x
-            ryH = Player.y
+            rxH = PlayerXcenter
+            ryH = PlayerYcenter
             dofH = MaxDof
         end
         
@@ -56,11 +63,12 @@ function DrawRays3D()
             local my = math.floor(ryH / MapTileSize)
             local mp = my * mapX + mx + 1
             
-            if mp > 0 and mp <= mapX * mapY and map[mp] == 1 then
+            if mp > 0 and mp <= mapX * mapY and map[mp] ~= 0 then
                 hRx = rxH
                 hRy = ryH
                 hHit = true
-                hDist = math.sqrt((Player.x - hRx)^2 + (Player.y - hRy)^2)
+                hWallType = map[mp]
+                hDist = math.sqrt((PlayerXcenter - hRx)^2 + (PlayerYcenter - hRy)^2)
                 dofH = MaxDof
             else
                 rxH = rxH + xoH
@@ -75,20 +83,18 @@ function DrawRays3D()
         local rxV, ryV, xoV, yoV = 0, 0, 0, 0
         
         if ra > math.pi/2 and ra < 3*math.pi/2 then -- Лево
-            rxV = (math.floor(Player.x / MapTileSize) * MapTileSize) - 0.0001
-            ryV = (Player.x - rxV) * nTan + Player.y
+            rxV = (math.floor(PlayerXcenter / MapTileSize) * MapTileSize) - 0.0001
+            ryV = (PlayerXcenter - rxV) * nTan + PlayerYcenter
             xoV = -MapTileSize
             yoV = -xoV * nTan
         elseif ra < math.pi/2 or ra > 3*math.pi/2 then -- Право
-            rxV = (math.floor(Player.x / MapTileSize) * MapTileSize) + MapTileSize
-            ryV = (Player.x - rxV) * nTan + Player.y
+            rxV = (math.floor(PlayerXcenter / MapTileSize) * MapTileSize) + MapTileSize
+            ryV = (PlayerXcenter - rxV) * nTan + PlayerYcenter
             xoV = MapTileSize
             yoV = -xoV * nTan
-        end
-        
-        if ra == 0 or ra == math.pi then
-            rxV = Player.x
-            ryV = Player.y
+        else
+            rxV = PlayerXcenter
+            ryV = PlayerYcenter
             dofV = MaxDof
         end
         
@@ -97,11 +103,12 @@ function DrawRays3D()
             local my = math.floor(ryV / MapTileSize)
             local mp = my * mapX + mx + 1
             
-            if mp > 0 and mp <= mapX * mapY and map[mp] == 1 then
+            if mp > 0 and mp <= mapX * mapY and map[mp] ~= 0 then
                 vRx = rxV
                 vRy = ryV
                 vHit = true
-                vDist = math.sqrt((Player.x - vRx)^2 + (Player.y - vRy)^2)
+                vWallType = map[mp]
+                vDist = math.sqrt((PlayerXcenter - vRx)^2 + (PlayerYcenter - vRy)^2)
                 dofV = MaxDof
             else
                 rxV = rxV + xoV
@@ -111,30 +118,35 @@ function DrawRays3D()
         end
         
         local finalRx, finalRy, finalDist
-        local wallColor = {0.8, 0.8, 0.8}
+        local wallType = 1
+        local isVerticalHit = false
+        local wallX -- Координата X на текстуре
         
         if hHit and vHit then
             if hDist < vDist then
                 finalRx, finalRy, finalDist = hRx, hRy, hDist
-                wallColor = {0.8, 0.8, 0.8}
+                wallType = hWallType
+                wallX = finalRx % MapTileSize
             else
                 finalRx, finalRy, finalDist = vRx, vRy, vDist
-                wallColor = {0.6, 0.6, 0.6}
+                wallType = vWallType
+                isVerticalHit = true
+                wallX = finalRy % MapTileSize
             end
         elseif hHit then
             finalRx, finalRy, finalDist = hRx, hRy, hDist
-            wallColor = {0.8, 0.8, 0.8}
+            wallType = hWallType
+            wallX = finalRx % MapTileSize
         elseif vHit then
             finalRx, finalRy, finalDist = vRx, vRy, vDist
-            wallColor = {0.6, 0.6, 0.6}
+            wallType = vWallType
+            isVerticalHit = true
+            wallX = finalRy % MapTileSize
         else
-            finalRx = Player.x + math.cos(ra) * MaxLength
-            finalRy = Player.y + math.sin(ra) * MaxLength
             finalDist = MaxLength
-            wallColor = {0.8, 0.8, 0.8}
         end
         
-        -- Отрисовка лучей (2д вид)
+		--Отрисовка лучей (2д вид)
         love.graphics.setColor(1, 0, 0)
         love.graphics.setLineWidth(1)
         love.graphics.line(
@@ -171,15 +183,41 @@ function DrawRays3D()
         
         local lineX = r * (Width3D / (FOV * detail))
         
-        -- Отрисовка 3д стены
-        love.graphics.setColor(wallColor[1], wallColor[2], wallColor[3])
-        love.graphics.setLineWidth(LineWidth)
-        love.graphics.line(
-            lineX + XRender3D, 
-            lineO, 
-            lineX + XRender3D, 
-            lineH + lineO
-        )
+        if wallTextures[wallType] and finalDist < MaxLength then
+            local texX = math.floor((wallX / MapTileSize) * textureSize)
+            if texX < 0 then texX = 0 end
+            if texX >= textureSize then texX = textureSize - 1 end
+            
+            if isVerticalHit then
+                wallColor = {0.75, 0.75, 0.75}
+			else
+				wallColor = {1, 1, 1}
+            end
+            
+            love.graphics.setColor(wallColor[1], wallColor[2], wallColor[3])
+            
+            love.graphics.draw(
+                wallTextures[wallType],
+                love.graphics.newQuad(
+                    texX, 0, 1, textureSize,
+                    textureSize, textureSize
+                ),
+                lineX + XRender3D - LineWidth/2,
+                lineO,
+                0,
+                LineWidth,
+                lineH / textureSize
+            )
+        else 
+            --love.graphics.setColor(1,1,1)
+            --love.graphics.setLineWidth(LineWidth)
+            --love.graphics.line(
+            --    lineX + XRender3D, 
+            --    lineO, 
+            --    lineX + XRender3D, 
+            --    lineH + lineO
+            --)
+        end
         
         ra = ra + DR
     end
