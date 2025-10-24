0cd8e3a311f7e00fbff51d0d87b8c01ea9b61d99
pravshaAa <141495151+pravshaAa@users.noreply.github.com>
2025-10-24 16:58:17 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 require"player"
 require"map"
 require"raycast"
+require"loadTexture"
 
 function love.load()
-	love.graphics.setBackgroundColor(0.3,0.3,0.3)--(0.06, 0.73, 0.9)  
-	Player = Player()
+    love.graphics.setBackgroundColor(0.3,0.3,0.3)
+    Player = Player()
+    loadTexture()
+    CreateMapTable()
 end
 
 function love.update(dt)
@@ -17,11 +20,21 @@ function love.update(dt)
 			Player.y = Player.oldY
 		end
 	end
+	
+	FPS = love.timer.getFPS()
 end
 
 function love.draw()
+	love.graphics.setColor(0.4,0.4,0.4)
+	love.graphics.rectangle("fill", 512, 256, 512, 256)
+	
+	love.graphics.setColor(0.06, 0.73, 0.9) 
+	love.graphics.rectangle("fill", 512, 0, 512, 256)
+	
 	DrawRays3D()
 	MapDraw()
 	Player:Draw()
+	
+	love.graphics.setColor(0, 0, 0)
+	love.graphics.print("FPS:"..tostring(FPS), 10, 20)
 end
-
